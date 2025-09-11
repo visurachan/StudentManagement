557d9f438d98258d25fdf4afafb94fa79e4db800
visurachan <visurachandula@gmail.com>
2025-09-12 00:25:42 +0100

    Created the student class for student management system

---
+++ src/main/java/com/example/StudentManagement/Student.java
@@ -0,0 +1,72 @@
+package com.example.StudentManagement;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+    private LocalDate dateOfBirth;
+    private Integer marks;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, String telephone, LocalDate dateOfBirth, Integer marks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.telephone = telephone;
+        this.dateOfBirth = dateOfBirth;
+        this.marks = marks;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Integer marks) {
+        this.marks = marks;
+    }
+}
---
+++ src/main/java/com/example/StudentManagement/StudentManagementController.java
@@ -0,0 +1,4 @@
+package com.example.StudentManagement;
+
+public class StudentManagementController {
+}
